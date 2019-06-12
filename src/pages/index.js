628fe67ae38bf0752cbc3b01af0b9a9c20c1904f
David Dugger <daviddugger@Davids-MacBook-Pro.local>
2019-06-11 21:45:14 -0400

    basic header for homepage complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,8 +6,6 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>David's Wallet</h1>
-    <p>Project initialized</p>
   </Layout>
 )
 
