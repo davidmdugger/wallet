f5e5925aa58dca800c5536f9d6d5d5a4bbbacd7f
David Dugger <davidmdj86@gmail.com>
2019-06-14 22:08:14 -0400

    reports, contact, about pages created (#6)

---
+++ src/pages/reports.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import HomeHeader from '../components/Globals/HomeHeader';
+
+export default function reports() {
+  return (
+    <div>
+      <HomeHeader />
+      <h2>reports</h2>
+    </div>
+  )
+}
