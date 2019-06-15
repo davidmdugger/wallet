e99fd6ed407cf6307b9cf7d0798fa2ae4363af43
David Dugger <daviddugger@Davids-MacBook-Pro.local>
2019-06-15 10:42:01 -0400

    added background component, fixed layout styling so header is fixed and all content below header starts after header DOM node so header doesn't cover other nodes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,33 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
+import BackgroundSection from '../components/Globals/BackgroundSection'
+import Card from '../components/Globals/Card'
+import SEO from "../components/seo"
+import { graphql } from 'gatsby';
+
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
+    <Card />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="Index Title"
+    />
   </Layout>
 )
+
+export const query = graphql`
+{
+  img:file(relativePath:{eq:"tradingview-chart.jpg"}){
+    childImageSharp{
+      fluid{
+        ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
+}
+`
 
 export default IndexPage
