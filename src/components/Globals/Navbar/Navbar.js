e64afa6fcd62d7910ded34b9ce7c08e40a698d1d
David Dugger <davidmdj86@gmail.com>
2019-06-16 10:24:22 -0400

    Styling fixes nav, card, home (#8)
    * create blog template created, basic blog component created, background section default styling moved to component's scss file
    
    * several styling fixes

--- src/components/Globals/Navbar/Navbar.js
+++ src/components/Globals/Navbar/Navbar.js
@@ -2,26 +2,9 @@ import React, { PureComponent, Fragment } from 'react'
 import { Link } from 'gatsby';
 import { FaBars, FaRegTimesCircle } from 'react-icons/fa';
 
-import './Navbar.scss';
+import { routes } from '../../../routes';
 
-const routes = [
-  {
-    path: '/',
-    text: 'home'
-  },
-  {
-    path: '/about',
-    text: 'about'
-  },
-  {
-    path: '/contact',
-    text: 'contact'
-  },
-  {
-    path: '/reports',
-    text: 'reports'
-  }
-]
+import './Navbar.scss';
 
 class Navbar extends PureComponent {
   state = {
@@ -54,6 +37,7 @@ class Navbar extends PureComponent {
   }
 
   render() {
+    console.log(routes)
     const menuStyle = this.state.isMenuExpanded ? "big" : "small";
     return (
       <nav onClick={this.toggleMenu} className={menuStyle}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,32 +2,45 @@ import React from "react"
 
 import Layout from "../components/layout"
 
-import BackgroundSection from '../components/Globals/BackgroundSection'
-import Card from '../components/Globals/Card'
 import SEO from "../components/seo"
-import { graphql } from 'gatsby';
+import { StaticQuery, graphql } from 'gatsby';
 
 const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="Home" />
-    <Card />
-    <BackgroundSection
-      img={data.img.childImageSharp.fluid}
-      title="Index Title"
-    />
-  </Layout>
-)
-
-export const query = graphql`
-{
-  img:file(relativePath:{eq:"tradingview-chart.jpg"}){
-    childImageSharp{
-      fluid{
-        ...GatsbyImageSharpFluid_tracedSVG
+  <StaticQuery
+    query={graphql`
+    query MyQuery {
+      allContentfulBlog {
+        edges {
+          node {
+            createdAt
+            title
+            content {
+              content
+            }
+          }
+        }
       }
     }
-  }
-}
-`
+    `}
+    render={({
+      allContentfulBlog: {
+        edges
+      },
+    }) => (
+        <Layout>
+          <SEO title="Home" />
+          <h1>david's wallet</h1>
+          <div>
+            {edges.map(e => (
+              <React.Fragment>
+                <p>{e.node.title}</p>
+              </React.Fragment>
+            ))}
+          </div>
+        </Layout>
+      )
+    }
+  />
+)
 
 export default IndexPage
